4d47cdb87f54db8845ec24bea0562442d3c428c6
Alaa Agbaria <agbaria.3laa@gmail.com>
2017-07-25 20:25:50 +0300

    Update dbConfig.js

--- utils/dbConfig.js
+++ utils/dbConfig.js
@@ -1,9 +1,9 @@
 var config = {
-	userName: process.env.username,
-	password: process.env.password,
-	server: process.env.server,
+	userName: 'agbaria', //process.env.username,
+	password: 'khalefA2', //process.env.password,
+	server: 'ip2017ex3.database.windows.net', //process.env.server,
 	options: {
-		database: process.env.database,
+		database: 'IP2017EX3DB', //process.env.database,
 		encrypt: true
 	}
 };
