320003fb010b04816cfb87d26ec88b49d4530b48
agbaria <agbaria.3laa@gmail.com>
2017-07-25 03:38:29 +0300

    dbconfig

---
+++ utils/dbConfig.js
@@ -0,0 +1,12 @@
+var config = {
+  userName: 'agbaria',
+  password: 'khalefA2',
+  server: 'ip2017ex3.database.windows.net',
+  options: {
+      database: 'IP2017EX3DB',
+      rowCollectionOnRequestCompletion: true,
+      encrypt: true
+  }
+};
+
+module.exports = config;
