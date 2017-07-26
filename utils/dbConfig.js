b6fa85c9e4ccb87e7f86bd550f0f0f54ced5118b
agbaria <agbaria.3laa@gmail.com>
2017-07-26 03:00:54 +0300

    some fixes

--- utils/dbConfig.js
+++ utils/dbConfig.js
@@ -1,10 +1,11 @@
 var config = {
-	userName: process.env.username,
-	password: process.env.password,
-	server: process.env.server,
+	userName: process.env.npm_package_config_username,
+	password: process.env.npm_package_config_password,
+	server: process.env.npm_package_config_server,
 	options: {
-		database: process.env.database,
-		encrypt: true
+		database: process.env.npm_package_config_database,
+		encrypt: true,
+		"rowCollectionOnRequestCompletion": true
 	}
 };
 
