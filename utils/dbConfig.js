d41d9bc9e66b0e04ce1e22a7c070c89306dfd30f
agbaria <agbaria.3laa@gmail.com>
2017-07-26 03:31:17 +0300

    fix

--- utils/dbConfig.js
+++ utils/dbConfig.js
@@ -1,9 +1,9 @@
 var config = {
-	userName: process.env.npm_package_config_username || process.env.username,
-	password: process.env.npm_package_config_password || process.env.password,
-	server: process.env.npm_package_config_server || process.env.server,
+	userName: process.env.username,
+	password: process.env.password,
+	server: process.env.server,
 	options: {
-		database: process.env.npm_package_config_database || process.env.database,
+		database: process.env.database,
 		encrypt: true,
 		"rowCollectionOnRequestCompletion": true
 	}
